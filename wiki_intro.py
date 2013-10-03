32c789e34df49c3b47cf1000593d41706c63c5eb
Monica Toth <mh.toth@gmail.com>
2013-10-03 10:27:16 -0400

    New changes let you add pages in rapid succession, keep track of how many have been added so far, and read them in a bulleted list at the bottom.

--- wiki_intro.py
+++ wiki_intro.py
@@ -3,28 +3,25 @@ import time, datetime
 
 app_wiki = Flask(__name__)
 
-app_wiki.vars = {}
 app_wiki.pages = {}
 
 @app_wiki.route('/', methods = ['GET', 'POST'])
 def hello():
 	if request.method == 'GET':
-		return render_template('hello_wiki.html')
+		return render_template('hello_wiki.html', num=0)
 	else:
 		# grabs the timestamp at the moment we receive the POST message
 		epoch_ts = time.time()
-		legible_ts = datetime.datetime.fromtimestamp(epoch_ts).strftime('%m - %d %H:%M')
+		legible_ts = datetime.datetime.fromtimestamp(epoch_ts).strftime('%m-%d %H:%M')
 		# now feeds this into app_wiki.pages
-		# note: identical titles will overwrite each other
-		app_wiki.pages['timestamp'] = epoch_ts
-		app_wiki.pages['content'] = [request.form['title'], request.form['content']]
+		app_wiki.pages[epoch_ts] = [request.form['title'], request.form['content']]
 		wiki_title = request.form['title']
 		wiki_content = request.form['content']
+		# wiki metadata
+		num = len(app_wiki.pages)
+		return render_template('hello_wiki.html', num=num, pages=app_wiki.pages)
 
-		return 'On: %s <br> You asked for title: %s <br> And you asked for content: %s'%(legible_ts, wiki_title, wiki_content)
-
-@app_wiki.route('/archive')
-def showall():
+		# return 'On: %s <br> You asked for title: %s <br> And you asked for content: %s'%(legible_ts, wiki_title, wiki_content)
 
 
 if __name__ == "__main__":
