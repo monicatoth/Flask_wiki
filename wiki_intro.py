7dd22762fc5c62187917d7b86c01af770f2682bb
Monica Toth <mh.toth@gmail.com>
2013-10-03 20:20:45 -0400

    Each wiki page now has its own link; an excerpt of <=100 characters is displayed on the front page

--- wiki_intro.py
+++ wiki_intro.py
@@ -4,6 +4,7 @@ import time, datetime
 app_wiki = Flask(__name__)
 
 app_wiki.pages = {}
+app_wiki.chronology = {}
 
 @app_wiki.route('/', methods = ['GET', 'POST'])
 def hello():
@@ -12,17 +13,28 @@ def hello():
 	else:
 		# grabs the timestamp at the moment we receive the POST message
 		epoch_ts = time.time()
-		legible_ts = datetime.datetime.fromtimestamp(epoch_ts).strftime('%m-%d %H:%M')
 		# now feeds this into app_wiki.pages
-		app_wiki.pages[epoch_ts] = [request.form['title'], request.form['content']]
 		wiki_title = request.form['title']
 		wiki_content = request.form['content']
+		wiki_excerpt = wiki_content[:100]
+		app_wiki.chronology[wiki_title] = epoch_ts
+		app_wiki.pages[wiki_title] = [wiki_excerpt, wiki_content]
 		# wiki metadata
 		num = len(app_wiki.pages)
-		return render_template('hello_wiki.html', num=num, pages=app_wiki.pages)
+		ordered_epochs = sorted(app_wiki.chronology.keys())
+		# display new content
+		return render_template('hello_wiki.html', num=num, order=ordered_epochs, pages=app_wiki.pages)
 
-		# return 'On: %s <br> You asked for title: %s <br> And you asked for content: %s'%(legible_ts, wiki_title, wiki_content)
+@app_wiki.route('/pages/<title>')
+def loadpage(title):
+	timestamp = app_wiki.chronology[title]
+	excerpt, content = app_wiki.pages[title]
+	legible_ts = datetime.datetime.fromtimestamp(timestamp).strftime('%m-%d %H:%M')
+	return render_template('wiki_page.html', title=title, content=content, dt=legible_ts)
 
+@app_wiki.route('/placeholder')
+def placeholder():
+	return render_template('dumb.html')
 
 if __name__ == "__main__":
 	app_wiki.run(debug = True)
